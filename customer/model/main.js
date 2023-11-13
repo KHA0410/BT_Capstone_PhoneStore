a6a86eeebb2418a38a5311982a976dd5f0ee6caf
KHA0410 <khanguyen151121@gmail.com>
2023-11-13 22:08:11 +0700

    CyberPhone day4

---
+++ customer/model/main.js
@@ -0,0 +1,23 @@
+class Phone {
+  constructor(
+    ma,
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    desc,
+    type
+  ) {
+    this.ma = ma;
+    this.name = name;
+    this.price = price;
+    this.screen = screen;
+    this.backCamera = backCamera;
+    this.frontCamera = frontCamera;
+    this.img = img;
+    this.desc = desc;
+    this.type = type;
+  }
+}
